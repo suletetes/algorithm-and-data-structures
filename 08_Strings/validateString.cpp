7352b2033aa24d527f6a011f81136182b644f019
suleiman <sulea841@gmail.com>
2023-05-16 19:14:10 +0100

    section 8 strings

--- 08_Strings/validateString.cpp
+++ 08_Strings/validateString.cpp
@@ -1,3 +1,20 @@
-//
-// Created by suleiman on 16/05/2023.
-//
+#include <iostream>
+#include "cstdlib"
+
+int valid(char *name) {
+    for (int i = 0; name[i] != '\0'; ++i) {
+        if (!(name[i] >= 65 && name[i] < 90) && !(name[i] >= 97 && name[i] <= 122) && !(name[i] >= 48 && name[i] <= 57))
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+//    char *name = "sggf2.";
+    char *name = "sule";
+    if (valid(name)) {
+        printf("Valid string");
+    } else {
+        printf("Invalid string");
+    }
+}
