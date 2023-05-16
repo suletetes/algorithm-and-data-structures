dcd0d52fd7b2e3c42c002ff7722728cc7b4695e5
suleiman <sulea841@gmail.com>
2023-05-16 18:08:14 +0100

    section 8 strings

--- 08_Strings/changingCaseString.cpp
+++ 08_Strings/changingCaseString.cpp
@@ -2,19 +2,32 @@
 #include "cstdlib"
 
 //specifically changing from uppper to lower case
-char changeCase(char arr []){
+char upperToLower(char arr[]) {
     int i;
-    for (i = 0; arr[i] != '\0' ; ++i) {
+    for (i = 0; arr[i] != '\0'; ++i) {
         arr[i] = arr[i] + 32;
     }
     printf("%s", arr);
 }
 
+char caseChanger(char arr[]) {
+    for (int i = 0; arr[i] != '\0'; ++i) {
+        if (arr[i] >= 65 && arr[i] <= 90)
+            arr[i] += 32;
+        else if (arr[i] >= 'a' && arr[i] <= 122)
+            arr[i] -= 32;
+    }
+    printf("%s", arr);
 
-int main(){
+}
+
+
+int main() {
 
     char A[] = "WELCOME";
-    changeCase(A);
+    upperToLower(A);
+    printf("\n %s \n", A);
+    caseChanger(A);
 
     return 0;
 }
