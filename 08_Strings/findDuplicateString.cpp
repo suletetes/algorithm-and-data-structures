9f8893b06a5670965c6ff0205a9d0985948ac3fe
suleiman <sulea841@gmail.com>
2023-05-17 01:26:14 +0100

    section 8 strings

--- 08_Strings/findDuplicateString.cpp
+++ 08_Strings/findDuplicateString.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 #include "cstdlib"
+
+void duplicatesString(char a[]){
+    int h[26], i;
+    for (i = 0; a[i] != '\0' ; ++i) {
+        h[a[i] - 97] ++;
+
+    }
+    for (i = 0;  i<26; i++) {
+        if (h[i] > 1){
+            printf("%c", i+97);
+            printf("%d", i+97);
+        }
+    }
+}
+
+int main(){
+    char b[] = "finding";
+    duplicatesString(b);
+}
+
+
+
+
+
