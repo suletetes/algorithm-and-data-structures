b8ccb82915a51d639c6b56b96199d7b9cadb1e85
suleiman <sulea841@gmail.com>
2023-05-18 19:15:23 +0100

    section 8 strings

--- 08_Strings/findDuplicateString.cpp
+++ 08_Strings/findDuplicateString.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 #include "cstdlib"
 
-void duplicatesString(char a[]){
+void duplicatesString(char a[]) {
     int h[26], i;
-    for (i = 0; a[i] != '\0' ; ++i) {
-        h[a[i] - 97] ++;
+    for (i = 0; a[i] != '\0'; ++i) {
+        h[a[i] - 97]++;
 
     }
-    for (i = 0;  i<26; i++) {
-        if (h[i] > 1){
-            printf("%c", i+97);
-            printf("%d", i+97);
+    for (i = 0; i < 26; i++) {
+        if (h[i] > 1) {
+            printf("%c", i + 97);
+            printf("%d", i + 97);
         }
     }
 }
 
-int main(){
+void duplicatesString2(char a[]) {
+    long int h = 0, x = 0;
+    int i;
+    for (i = 0; a[i] != '\0'; ++i) {
+        x = 1;
+        x = x << (a[i] - 97); // asci code starting from small letter a is 97
+    }
+    if ((x & h) > 0) {
+        printf("%c is duplicate", a[i]);
+    } else {
+        h = x:h;
+    }
+}
+
+int main() {
     char b[] = "finding";
     duplicatesString(b);
 }
