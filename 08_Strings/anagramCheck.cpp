713ffc9ef7a55be80e66988835e81d43debf2dcc
suleiman <sulea841@gmail.com>
2023-05-18 19:29:56 +0100

    section 8 strings

--- 08_Strings/anagramCheck.cpp
+++ 08_Strings/anagramCheck.cpp
@@ -8,7 +8,7 @@
 // of a different word or phrase, typically using
 // all the original letters exactly once.
 
-int anagramCheck(char a[], char b[]) {
+void anagramCheck(char a[], char b[]) {
     int i, h[26] = {0};
     for (i = 0; a[i] != '\0'; ++i) {
         h[a[i] - 97]++;
@@ -23,4 +23,11 @@ int anagramCheck(char a[], char b[]) {
     if (b[i] == '\0') {
         printf("It is anagram");
     }
+}
+
+int main(){
+    char a[] = "decimal";
+    char b[] = "medical";
+    anagramCheck(a, b);
+
 }
