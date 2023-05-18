5df33cc6a142f018fdf2541090c553adc0d2ba6e
suleiman <sulea841@gmail.com>
2023-05-18 19:21:31 +0100

    section 8 strings

--- 08_Strings/anagramCheck.cpp
+++ 08_Strings/anagramCheck.cpp
@@ -1,3 +1,22 @@
+#include "iostream"
+#include "stdio.h"
+
+
+
+
 // An anagram is a word or phrase formed by rearranging the letters
 // of a different word or phrase, typically using
 // all the original letters exactly once.
+
+int anagramCheck(char a[], char b[]) {
+    int i, h[26] = {0};
+    for (i = 0; a[i] != '\0'; ++i) {
+        h[a[i] - 97]++;
+    }
+    for (i = 0; b[i] != '\0'; i++) {
+        h[a[i] - 97]--;
+    }
+    if (h[a[i] -97] <0){
+
+    }
+}
