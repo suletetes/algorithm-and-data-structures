bd2bbda6ab0da92fd2e870b82c00c84f41595f4c
suleiman <sulea841@gmail.com>
2023-05-19 17:42:43 +0100

    section 8 strings

--- 08_Strings/permutationString.cpp
+++ 08_Strings/permutationString.cpp
@@ -1,3 +1,39 @@
+#include <iostream>
+#include "cstdlib"
+
+//void perm(char s[], int l, int h){
+//    int i;
+//    if (l ==h){
+//        printf("%s", s);
+//    }else{
+//        for (i = l; i <= h ; ++i) {
 //
-// Created by suleiman on 19/05/2023.
-//
+//        }
+//    }
+//}
+
+
+void perm(char s[], int k) {
+    static int a[10] = {0};
+    static char res[10];
+    int i;
+    if (s[k] == '\0') {
+        res[k] == '\0';
+        printf("%s", res);
+    } else {
+        for (i = 0; s[i] != '\0'; ++i) {
+            if (a[i] == 0) {
+                res[k] = s[i];
+                a[i] = 1;
+                perm(s, k + 1);
+                a[i] = 0;
+            }
+        }
+    }
+}
+
+
+int main(){
+    char s[] = "ABC";
+    perm(s, 0);
+}
