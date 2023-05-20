a5195e83257c8ddb52ecd2bc77ac1ea157f9bd15
suleiman <sulea841@gmail.com>
2023-05-20 17:21:44 +0100

    section 9 matrices

--- 09_matrices/diagonalMatrix.cpp
+++ 09_matrices/diagonalMatrix.cpp
@@ -23,14 +23,23 @@ void display(struct matrix m) {
     for (i = 0; i < m.n; ++i) {
         for (j = 0; j < m.n; ++j) {
             if (i == j) printf("%d", m.a[i - 1]);
+            else printf("0 ");
         }
+        printf("\n");
     }
 }
 
 
 int main() {
     struct matrix m;
+    m.n = 4;
+    set(&m, 1, 1, 5);
+    set(&m, 2, 2, 5);
+    set(&m, 3, 3, 5);
+    set(&m, 4, 4, 5);
+    display(m);
+    printf("%d \n", get(m, 2, 2));
 
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
