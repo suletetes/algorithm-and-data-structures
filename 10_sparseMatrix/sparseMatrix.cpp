42971955b017625a0626e22d49121f626682c19e
suleiman <sulea841@gmail.com>
2023-05-21 23:58:07 +0100

    section 10 sparse matrix and polynomials

--- 10_sparseMatrix/sparseMatrix.cpp
+++ 10_sparseMatrix/sparseMatrix.cpp
@@ -21,14 +21,28 @@ void create(struct sparse *s) {
     scanf("%d", &s->num);
     s->e = new element[s->num];
     for (i = 0; i < s->num; ++i) {
-        scanf("%d%d%d", &s->e[i].i,  &s->e[i].j,  &s->e[i].x);
+        scanf("%d%d%d", &s->e[i].i, &s->e[i].j, &s->e[i].x);
     }
 
+}
+
+void display(struct sparse s) {
+    int i, j, k = 0;
+    for (i = 0; i < s.m; ++i) {
+        for (j = 0; j < s.n; ++j) {
+            if (i == s.e[k].i && j == s.e[k].j)
+                printf("%d ", s.e[k++].x);
+            else
+                printf("0 ");
+        }
+        printf("\n");
+    }
 }
 
 
 int main() {
     struct sparse s;
     create(&s);
+    display(s);
     return 0;
 }
