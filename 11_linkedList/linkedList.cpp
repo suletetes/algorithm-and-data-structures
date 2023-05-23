907a854961cd7b5ef31994a3a05893f4b8ef8d81
suleiman <sulea841@gmail.com>
2023-05-24 00:05:46 +0100

    section 11 linked List

--- 11_linkedList/linkedList.cpp
+++ 11_linkedList/linkedList.cpp
@@ -10,9 +10,27 @@ void create(int a[], int n) {
     int i;
     struct node *t, *last;
     first = (struct node *) malloc(sizeof(struct node));
+    first->data = a[0];
+    first->next = NULL;
+    last = first;
+    for (i = 1; i < n; ++i) {
+        t = (struct node *) malloc(sizeof(struct node));
+        t->data = a[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct node *p) {
+    while (p != NULL) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
 }
 
 int main() {
     int a[] = {3, 5, 7, 10, 15};
+    create(a, 5);
     return 0;
 }
