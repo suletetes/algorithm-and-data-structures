81465ee4972d657c315032a166baac150bbecee7
suleiman <sulea841@gmail.com>
2023-05-23 23:59:46 +0100

    section 11 linked List

--- 11_linkedList/linkedList.cpp
+++ 11_linkedList/linkedList.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "cstdlib"
+
+struct node {
+    int data;
+    struct node *next;
+} *first = NULL;
+
+void create(int a[], int n) {
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a[] = {3, 5, 7, 10, 15};
     return 0;
 }
