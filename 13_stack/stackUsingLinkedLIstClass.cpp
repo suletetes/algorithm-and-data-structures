7cc0da3c5f0651de385f2917b42379ab5762d02e
suleiman <sulea841@gmail.com>
2023-05-31 15:48:05 +0100

    section 13 stack

--- 13_stack/stackUsingLinkedLIstClass.cpp
+++ 13_stack/stackUsingLinkedLIstClass.cpp
@@ -1,3 +1,49 @@
-//
-// Created by suleiman on 30/05/2023.
-//
+#include "iostream"
+
+using namespace std;
+
+class node {
+public:
+    int data;
+    node *next;
+};
+
+class stack {
+private:
+    node *top;
+public:
+    stack() {
+        top = NULL;
+    }
+
+    void push(int x);
+
+    int pop();
+
+    void display();
+};
+
+void stack::push(int x) {
+    node *t = new node;
+    if (t == NULL)
+        cout << "stack is FULL";
+    else {
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+
+}
+
+int stack::pop() {
+    int x = -1;
+    if (top == NULL)
+        cout << "stack is empty";
+    else {
+        x = top->data;
+        node *t = top;
+        top = top->next;
+        delete t;
+    }
+    return x;
+}
