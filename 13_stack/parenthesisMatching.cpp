22f20cfdf87f966e23d2f2ab5464bb9ff24125a6
suleiman <sulea841@gmail.com>
2023-05-31 16:09:48 +0100

    section 13 stack

--- 13_stack/parenthesisMatching.cpp
+++ 13_stack/parenthesisMatching.cpp
@@ -1,3 +1,52 @@
-//
-// Created by suleiman on 30/05/2023.
-//
+#include "stdio.h"
+#include "stdlib.h"
+
+struct node {
+    char data;
+    struct node *next;
+} *top = NULL;
+
+void push(char x) {
+    struct node *t;
+    t = (struct node *) malloc(sizeof(struct node));
+    if (t == NULL) {
+        printf("stack is full\n");
+    } else {
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+}
+
+char pop() {
+    struct node *t;
+    char x = -1;
+    if (top == NULL) {
+        printf("Stack is empty\n");
+    } else {
+        t = top;
+        top = top->next;
+        x = t->data;
+        free(t);
+    }
+    return x;
+}
+
+void display() {
+    struct node *p;
+    p = top;
+    while (p != NULL) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    display();
+    printf("%d ", pop());
+    return 0;
+}
