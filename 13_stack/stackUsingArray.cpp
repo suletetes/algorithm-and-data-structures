c7c5eb6684de667878e3b2f1e59e68d2acc2bbf9
suleiman <sulea841@gmail.com>
2023-05-31 14:28:36 +0100

    section 13 stack

--- 13_stack/stackUsingArray.cpp
+++ 13_stack/stackUsingArray.cpp
@@ -1,5 +1,16 @@
  #include <iostream>
-
+#include <stdio.h>
+struct stack{
+    int size;
+    int top;
+    int *s;
+};
+void create(struct stack *st){
+    printf("Enter size");
+    scanf("%d", &st->size);
+    st->top =-1;
+    st-> s= (int *)malloc(st->size*sizeof(int));
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
