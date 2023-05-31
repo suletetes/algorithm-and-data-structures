e89b377a7100cff976a8a6c875f56cf0ebcc4725
suleiman <sulea841@gmail.com>
2023-05-31 14:38:03 +0100

    section 13 stack

--- 13_stack/stackUsingArray.cpp
+++ 13_stack/stackUsingArray.cpp
@@ -1,41 +1,47 @@
- #include <iostream>
+#include <iostream>
 #include <stdio.h>
 #include  "stdlib.h"
-struct stack{
+
+struct stack {
     int size;
     int top;
     int *s;
 };
-void create(struct stack *st){
+
+void create(struct stack *st) {
     printf("Enter size");
     scanf("%d", &st->size);
-    st->top =-1;
-    st-> s= (int *)malloc(st->size*sizeof(int));
+    st->top = -1;
+    st->s = (int *) malloc(st->size * sizeof(int));
 }
-void display(struct stack st){
+
+void display(struct stack st) {
     int i;
-    for(i = st.top;i>=0;i-- )
+    for (i = st.top; i >= 0; i--)
         printf("%d ", st.s[i]);
     printf("\n");
 
 }
-void push(struct stack *st, int x){
-    if(st->top==st->size-1)
+
+void push(struct stack *st, int x) {
+    if (st->top == st->size - 1)
         printf("stack overflow");
-    else{
+    else {
         st->top++;
-        st->s[st->top] =x;
+        st->s[st->top] = x;
     }
 }
-int pop(struct stack *st){
-    int x =-1;
-    if(st->top==-1)
+
+int pop(struct stack *st) {
+    int x = -1;
+    if (st->top == -1)
         printf("stack underflow\n");
-    else{
+    else {
         x = st->s[st->top--];
     }
     return x;
 }
+
 int main() {
     struct stack st;
     create(&st);
@@ -44,5 +50,6 @@ int main() {
     push(&st, 70);
     push(&st, 30);
     push(&st, 50);
+    display(st);
     return 0;
 }
