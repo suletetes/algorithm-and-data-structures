85f69d3368346fcdd2a9208cf9e192397b43e761
suleiman <sulea841@gmail.com>
2023-06-01 17:21:26 +0100

    section 14 queues

--- 14_queues/queueUsingArray.cpp
+++ 14_queues/queueUsingArray.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "stdio.h"
+
+struct queue {
+    int size;
+    int front;
+    int rear;
+    int *q;
+};
+
+void create(struct queue *q, int size) {
+    q->size = size;
+    q->front = q->rear = -1;
+    q->q = (int *) malloc(q->size * sizeof(int));
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    struct queue q;
+    create(&q, 5);
     return 0;
 }
