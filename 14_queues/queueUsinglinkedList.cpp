cc3c875a39c555510b8950e32f6621408530ce42
suleiman <sulea841@gmail.com>
2023-06-01 18:02:09 +0100

    section 14 queues

--- 14_queues/queueUsinglinkedList.cpp
+++ 14_queues/queueUsinglinkedList.cpp
@@ -1,3 +1,49 @@
-//
-// Created by suleiman on 01/06/2023.
-//
+#include "stdio.h"
+#include "stdlib.h"
+
+struct node {
+    int data;
+    struct node *next;
+} *front = NULL, *rear = NULL;
+
+
+void enqueue(int x) {
+    struct node *t;
+    t = (struct node *) malloc(sizeof(struct node));
+    if (t == NULL)
+        printf("queue is full");
+    else {
+        t->data = x;
+        t->next = NULL;
+        if (front == NULL)
+            front = rear = t;
+        else {
+            rear->next = t;
+            rear = t;
+        }
+
+    }
+}
+
+int dequeue() {
+    int x = -1;
+    struct node *t;
+    if (front == NULL)
+        printf("queue is empty");
+    else {
+        x = front->data;
+        t = front;
+        front = front->next;
+        free(t);
+    }
+    return x;
+}
+
+void display(){
+    struct node
+}
+
+
+int main() {
+
+}
