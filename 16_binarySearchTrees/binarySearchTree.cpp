a5f3402ad51a58db91ea55343124b7aba5726bc6
suleiman <sulea841@gmail.com>
2023-06-04 19:21:59 +0100

    section 16 binary search trees (bst)

--- 16_binarySearchTrees/binarySearchTree.cpp
+++ 16_binarySearchTrees/binarySearchTree.cpp
@@ -8,7 +8,23 @@ struct node {
 } *root = NULL;
 
 void insert(int key) {
+    struct node *t = root;
+    struct node *r, *p;
+    if (root == NULL) {
+        p = (struct node *) malloc(sizeof(struct node));
+        p->data = key;
+        p->lchild = p->rchild = NULL;
+    }
+    while (t != NULL) {
+        r = t;
+        if (key < t->data)
+            t = t->lchild;
+        else if (key > t->data)
+            t = t->rchild;
+        else
 
+            return
+    }
 }
 
 int main() {
