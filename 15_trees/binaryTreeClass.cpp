00a37100126702914a36071eb7c0350087c8d12f
suleiman <sulea841@gmail.com>
2023-06-04 01:12:55 +0100

    section 15  trees

--- 15_trees/binaryTreeClass.cpp
+++ 15_trees/binaryTreeClass.cpp
@@ -18,7 +18,46 @@ public:
     void postOrder(node *p);
 
     void inOrder(node *p);
+
+    void levelOrder(node *p);
+
+    void height(node *root);
 };
+
+void tree::createTree() {}() {
+    Node *root;
+    int x;
+    queue q(100);
+//    create(&q, 100);
+
+    printf("enter root value ");
+    scanf("%d", &x);
+    root = new Node;
+    root->data = x;
+    root->lchild = root->rchild = NULL;
+    q.enqueue(&q, root);
+    while (!isEmpty(q)) {
+        p = dequeue(&q);
+        printf("Enter left child of %d ", p->data);
+        scanf("%d", &x);
+        if (x != -1) {
+            t = (struct Node *) malloc(sizeof(struct Node));
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->lchild = t;
+            enqueue(&q, t);
+        }
+        printf("Enter right child of %d ", p->data);
+        scanf("%d", &x);
+        if (x != -1) {
+            t = (struct Node *) malloc(sizeof(struct Node));
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->rchild = t;
+            enqueue(&q, t);
+        }
+    }
+}
 
 
 int main() {
