c66b0a22e7704d08c92c7359c53e5561aad88ec9
suleiman <sulea841@gmail.com>
2023-06-05 17:21:07 +0100

    section 17 AVL Trees

--- 17_avlTrees/avlTrees.cpp
+++ 17_avlTrees/avlTrees.cpp
@@ -8,11 +8,19 @@ struct node {
     struct node *rchild;
 } *root = NULL;
 
+int nodeHeight(struct node *p) {
+    int hl, hr;
+    hl = p && p->lchild ? p->lchild->height : 0;
+    hr = p && p->rchild ? p->rchild->height : 0;
+    return hl > hr ? hl + 1 : hr + 1;
+}
+
 struct node *rInsert(struct node *p, int key) {
     struct node *t = NULL;
     if (p == NULL) {
         t = (struct node *) malloc(sizeof(struct node));
         t->data = key;
+        t->height = 1;
         t->lchild = t->rchild = NULL;
         return t;
     }
@@ -20,6 +28,7 @@ struct node *rInsert(struct node *p, int key) {
         p->lchild = rInsert(p->lchild, key);
     else if (key > p->data)
         p->rchild = rInsert(p->rchild, key);
+    p->height = nodeHeight(p);
     return p;
 }
 
