6cb2072544e496a16181c0ffe4a2d64bc57e23ff
suleiman <sulea841@gmail.com>
2023-06-07 00:19:10 +0100

    section 18 search trees

--- 18_searchTress/2_3tree_augmented.h
+++ 18_searchTress/2_3tree_augmented.h
@@ -4,5 +4,56 @@
 
 #ifndef INC_18_SEARCHTRESS_2_3TREE_AUGMENTED_H
 #define INC_18_SEARCHTRESS_2_3TREE_AUGMENTED_H
+#ifndef B2_3TREE_AUGMENTED_H
+#define B2_3TREE_AUGMENTED_H
 
+/**
+ * Implementation of 2-3 tree.
+ * Nizomiddin Toshpulatov <tremul27@gmail.com>
+ * 2020, FIIT STU, Bratislava, Slovakia
+ */
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include "treeio.h"
+
+#define ORDER 3
+#define INDENT_INC 4
+
+#define __indent(current) ((current) + INDENT_INC)
+
+#define __ASSIGN_SORTED_KEYS(a, b, c) (arr[0] = a, arr[1] = b, arr[2] = c)
+
+#define __DESTRUCT_MERGE_ARGS__					\
+	struct _node *node		= args.node;		\
+	struct _node *left		= args.left;		\
+	struct _node *middle	= args.middle;		\
+	int prom_key			= args.prom_key;	\
+
+struct _node {
+    bool isfull;
+    int low_key;
+    int high_key;
+    struct _node *left, *middle, *right;
+    struct _node *parent;
+};
+
+struct _merge_args {
+    struct _node *node, *left, *middle;
+    int prom_key;
+};
+
+extern struct _node *_node(struct _node init);
+extern struct _node *_update_node(struct _node *node, struct _node update);
+extern int *_sort_keys(int a, int b, int c);
+extern struct _node *_merge_with_parent(struct _merge_args args);
+extern struct _node *_split(struct _node *node, int key);
+extern struct _node *_add_key(struct _node *node, int key);
+extern struct _node *_get_root(struct _node *node);
+extern struct _node *_insert_key(struct _node *node, int key);
+extern struct _node *search(struct _node *node, int key);
+extern struct _node *insert(struct _node *node, int key);
+extern void clear(struct _node *node);
+
+#endif
 #endif //INC_18_SEARCHTRESS_2_3TREE_AUGMENTED_H
---
+++ 18_searchTress/treeio.h
@@ -0,0 +1,51 @@
+//
+// Created by suleiman on 07/06/2023.
+//
+
+#ifndef INC_18_SEARCHTRESS_TREEIO_H
+#define INC_18_SEARCHTRESS_TREEIO_H
+#ifndef TREEIO_H
+#define TREEIO_H
+
+#include <cstdio>
+
+#define CLR_RED     "\x1b[31m"
+#define CLR_GREEN   "\x1b[32m"
+#define CLR_YELLOW  "\x1b[33m"
+#define CLR_BLUE    "\x1b[34m"
+#define CLR_MAGENTA "\x1b[35m"
+#define CLR_CYAN    "\x1b[36m"
+#define CLR_RESET   "\x1b[0m"
+
+#define UNKNOWN_CMD_ERR "Unknown command"
+#define UNDEF_FUNC_T    "No defined function for the type"
+
+#define EXIT_MSG        "Exiting...\n"
+#define RET_TO_ROOT_MSG    "Returning to the root node\n"
+#define NODE_FOUND_MSG    "Node found"
+#define NODE_NFOUND_MSG    "Node not found"
+
+#define ENOMEM "Insufficent memory, cannot allocate node"
+#define ENONODE "Node is NULL, nothing to print"
+#define EDUPNODE "Duplicate node insertion. Operation rejected"
+
+#define COMMANDS CLR_CYAN "Commands:\n\th - print commands\n\ti [...values] - insert keys\n\ts [value] - search for value\n\tt - run insert and search tests from \n\t\t./data/randnumbers file.\n\t\tResults will be written to the ./data/timestampts file.\n\tp - print tree\n\tr - reset tree\n\tq - exit program\n--------------------------\n" CLR_RESET
+
+#define TESTS_COMPLETE_MSG CLR_YELLOW "\n>> Tests complete. Results written to the ./data/timestampts file --\n" CLR_RESET
+
+#define WELCOME_MSG CLR_MAGENTA "-- Welcome to self-balancing bst interaction tool!\n" CLR_RESET COMMANDS
+
+
+#define EXIT_FUNC        (printf(CLR_BLUE "--Exiting %s\n" CLR_RESET, __func__))
+#define throw(error)    (printf(CLR_RED "--Error: %s--" CLR_YELLOW " in %s\n" CLR_RESET, error, __func__))
+
+#define msg(...)        (printf(__VA_ARGS__))
+
+#define putws(n) {        \
+    typeof(n) _n = n;    \
+    while (_n-- > 0)    \
+        putchar(' ');    \
+}
+
+#endif
+#endif //INC_18_SEARCHTRESS_TREEIO_H
