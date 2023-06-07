a06116f521f4d5f4ce29e4d882ce4bb32d34ce25
suleiman <sulea841@gmail.com>
2023-06-07 16:58:32 +0100

    section 19 heap

--- 19_heap/heapSort.cpp
+++ 19_heap/heapSort.cpp
@@ -16,6 +16,7 @@ int Delete(int a[], int n) {
     val = a[1];
     x = a[n];
     a[1] = a[n];
+    a[n] = val;
     i = 1;
     j = i * 2;
     while (j < n - 1) {
@@ -37,8 +38,9 @@ int main() {
     int h[] = {0, 2, 5, 8, 9, 4, 10, 7};
     for (int i = 2; i <= 7; i++)
         insert(h, i);
-
-    printf("Deleted value is %d\n", Delete(h, 7));
+    for (int i = 7; i > 1; i--) {
+        printf("Deleted value is %d\n", Delete(h, i));
+    }
     for (int i = 1; i <= 7; ++i) {
         printf("%d ", h[i]);
     }
