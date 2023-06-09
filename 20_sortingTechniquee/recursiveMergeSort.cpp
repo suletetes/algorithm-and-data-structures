89103e1798014a3d8992bdb64439b8c903d16ab8
suleiman <sulea841@gmail.com>
2023-06-09 17:52:43 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/recursiveMergeSort.cpp
+++ 20_sortingTechniquee/recursiveMergeSort.cpp
@@ -1,3 +1,19 @@
-//
-// Created by suleiman on 08/06/2023.
-//
+#include <iostream>
+#include "stdio.h"
+
+void merge(int A[], int l, int mid, int h) {
+    int i = l, j = mid + 1, k = l;
+    int B[100];
+    while (i <= mid && j <= h) {
+        if (A[i] < A[j])
+            B[k++] = A[i++];
+        else
+            B[k++] = A[j++];
+    }
+    for (; i <= mid; i++)
+        B[k++] = A[i];
+    for (; j <= h; j++)
+        B[k++] = A[j];
+    for (i = l; i <= h; i++)
+        A[i] = B[i];
+}
