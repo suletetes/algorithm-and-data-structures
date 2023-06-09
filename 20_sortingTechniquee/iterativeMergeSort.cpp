7ecf487dae1729fd8712c309767d624c5131bec5
suleiman <sulea841@gmail.com>
2023-06-09 17:39:08 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/iterativeMergeSort.cpp
+++ 20_sortingTechniquee/iterativeMergeSort.cpp
@@ -10,7 +10,7 @@ void swap(int *x, int *y) {
 
 
 void merge(int a[], int l, int mid, int h) {
-    int i, j, k;
+    int i = l, j = mid + 1, k = l;
     int b[100];
     while (i <= mid && j <= h) {
         if (a[i] < a[j])
@@ -30,7 +30,7 @@ void merge(int a[], int l, int mid, int h) {
 
 }
 
-void iterativeSort(int a[], int n) {
+void iterativeMergeSort(int a[], int n) {
     int p, l, h, mid, i;
     for (p = 2; p <= n; p = p * 2) {
         for (i = 0; i + p - 1 < n; i = i + p) {
@@ -41,13 +41,13 @@ void iterativeSort(int a[], int n) {
         }
     }
     if (p / 2 < n)
-        merge(a, 0, p / 2, n - 1);
+        merge(a, 0, p / 2, n );
 }
 
 int main() {
 
-    int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2, INT32_MAX}, n = 11;
-
+    int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
+    iterativeMergeSort(a, n);
     for (int i = 0; i < 10; ++i) {
         printf("%d ", a[i]);
     }
