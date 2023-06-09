608f31b72c26512b0811eccf032066f06fe84563
suleiman <sulea841@gmail.com>
2023-06-09 17:34:01 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/iterativeMergeSort.cpp
+++ 20_sortingTechniquee/iterativeMergeSort.cpp
@@ -8,17 +8,6 @@ void swap(int *x, int *y) {
     *y = temp;
 }
 
-int partition(int a[], int l, int h) {
-    int pivot = a[l];
-    int i = l, j = h;
-    do {
-        do { ++i; } while (a[i] <= pivot);
-        do { --j; } while (a[i] > pivot);
-        if (i < j) swap(&a[i], &a[j]);
-    } while (i < j);
-    swap(&a[l], &a[j]);
-    return j;
-}
 
 void merge(int a[], int l, int mid, int h) {
     int i, j, k;
@@ -39,6 +28,20 @@ void merge(int a[], int l, int mid, int h) {
         a[i] = b[i];
     }
 
+}
+
+void iterativeSort(int a[], int n) {
+    int p, l, h, mid, i;
+    for (p = 2; p <= n; p = p * 2) {
+        for (i = 0; i + p - 1 < n; i = i + p) {
+            l = i;
+            h = i + p - 1;
+            mid = (l + h) / 2;
+            merge(a, l, mid, h);
+        }
+    }
+    if (p / 2 < n)
+        merge(a, 0, p / 2, n - 1);
 }
 
 int main() {
