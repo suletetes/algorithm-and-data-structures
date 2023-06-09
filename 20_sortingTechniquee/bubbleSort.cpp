35def0596fa94aca43bfaceb06e45fe7fc8ad720
suleiman <sulea841@gmail.com>
2023-06-09 15:16:12 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/bubbleSort.cpp
+++ 20_sortingTechniquee/bubbleSort.cpp
@@ -20,5 +20,10 @@ void bubble(int a[], int n) {
 int main() {
 
     int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
+    bubble(a, 10);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
     return 0;
 }
