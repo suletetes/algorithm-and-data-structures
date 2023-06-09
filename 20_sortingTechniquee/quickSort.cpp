c51f97815b88e5be2c66c101e63b099dc0f24541
suleiman <sulea841@gmail.com>
2023-06-09 15:41:36 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/quickSort.cpp
+++ 20_sortingTechniquee/quickSort.cpp
@@ -1,3 +1,32 @@
-//
-// Created by suleiman on 08/06/2023.
-//
+#include <iostream>
+#include "stdio.h"
+
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int partition(int a[], int l, int h) {
+    int pivot = a[l];
+    int i = l, j = h;
+    do {
+        do { ++i; } while (a[i] <= pivot);
+        do { --j; } while (a[i] > pivot);
+        if (i < j) swap(&a[i], &a[j]);
+    } while (i < j);
+    swap(&a[l], &a[j]);
+    return j;
+}
+
+void quickSort(int a[], int l, int h) {
+    int j;
+    if (l < h) {
+        j = partition(a, l, h);
+        quickSort(a, l, j);
+        quickSort(a, j + 1, h);
+    }
+}
+
+
