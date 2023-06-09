f2f994f667eb62d5f4ef70189cebcac2287f1116
suleiman <sulea841@gmail.com>
2023-06-09 15:31:13 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/selectionSort.cpp
+++ 20_sortingTechniquee/selectionSort.cpp
@@ -1,3 +1,23 @@
-//
-// Created by suleiman on 07/06/2023.
-//
+#include <iostream>
+#include "stdio.h"
+
+
+void selectionSort(int a[], int n) {
+    int i, j, k;
+    for (i = 1; i < n; ++i) {
+        for (j = k = i; j < n; ++j) {
+
+        }
+    }
+}
+
+int main() {
+
+    int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
+    selectionSort(a, n);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
