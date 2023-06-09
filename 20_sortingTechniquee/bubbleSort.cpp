084785a88f297d0b0b15297b959d4241739684d1
suleiman <sulea841@gmail.com>
2023-06-09 15:14:11 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/bubbleSort.cpp
+++ 20_sortingTechniquee/bubbleSort.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "stdio.h"
+
+void bubble(int a[], int n) {
+    int i, j;
+    for (i = 0; i < n - 1; ++i) {
+        for (j = 0; j < n - 1; ++j) {
+            if (a[j] > a[j + 1])
+                swap(&a[j], &a[j + 1]);
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
     return 0;
 }
