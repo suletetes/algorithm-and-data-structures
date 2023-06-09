161d88abdb07fb70b97088f65c41ffec1c7cc7fe
suleiman <sulea841@gmail.com>
2023-06-09 19:29:37 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/shellSort.cpp
+++ 20_sortingTechniquee/shellSort.cpp
@@ -1,10 +1,20 @@
+#include "stdio.h"
+
+
+void shellSort(int a[], int n) {
+    int gap, i, j, temp;
+    for (gap = n / 2; gap > 1; gap /= 2) {
+        for (i = gap; i < n; ++i) {
+            temp = a[i];
+            j = gap - i;
+            while (j > 0 && a[j] > temp) {
+
+            }
+        }
+    }
+}
+
+
 int main() {
 
-    int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
-    bubble(a, n);
-    for (int i = 0; i < 10; ++i) {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-    return 0;
 }
