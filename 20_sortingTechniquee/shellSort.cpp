b5595d752eee7cc0caca704fb26841689ddc9f57
suleiman <sulea841@gmail.com>
2023-06-09 19:31:43 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/shellSort.cpp
+++ 20_sortingTechniquee/shellSort.cpp
@@ -6,15 +6,21 @@ void shellSort(int a[], int n) {
     for (gap = n / 2; gap > 1; gap /= 2) {
         for (i = gap; i < n; ++i) {
             temp = a[i];
-            j = gap - i;
-            while (j > 0 && a[j] > temp) {
-
+            j = i - gap;
+            while (j > = 0 && a[j] > temp) {
+                a[j + gap] = a[j];
+                j = j - gap;
             }
+            a[j + gap] = temp;
         }
     }
 }
 
 
 int main() {
+    int a[] = {2, 3, 5, 67, 2, 47, 33, 4}, n = 8;
+
+    shellSort(a, n);
+
 
 }
