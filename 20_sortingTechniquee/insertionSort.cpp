b4436de8e358593a9280338345a6c97656e03b7f
suleiman <sulea841@gmail.com>
2023-06-09 15:26:29 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/insertionSort.cpp
+++ 20_sortingTechniquee/insertionSort.cpp
@@ -1,3 +1,27 @@
-//
-// Created by suleiman on 07/06/2023.
-//
+#include <iostream>
+#include "stdio.h"
+
+
+void insertionSort(int a[], int n) {
+    int i, j, x;
+    for (i = 1; i < n; ++i) {
+        j = i - 1;
+        x = a[i];
+        while (j > -1 && a[j] > x) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = x;
+    }
+}
+
+int main() {
+
+    int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
+    insertionSort(a, n);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
