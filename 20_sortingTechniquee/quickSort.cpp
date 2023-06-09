08c022ca285513fd9180c30500822ab6db4f1926
suleiman <sulea841@gmail.com>
2023-06-09 15:41:43 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/quickSort.cpp
+++ 20_sortingTechniquee/quickSort.cpp
@@ -29,4 +29,13 @@ void quickSort(int a[], int l, int h) {
     }
 }
 
+int main() {
 
+    int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2, INT_MAX}, n = 11;
+    quickSort(a, 0, n);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
