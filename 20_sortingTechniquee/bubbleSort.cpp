b3cf7536a5803901c7b6906288eff354b726f503
suleiman <sulea841@gmail.com>
2023-06-09 19:18:50 +0100

    Revert "section 20 sorting technique"
    This reverts commit 28572cb1

---
+++ 20_sortingTechniquee/bubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "stdio.h"
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void bubble(int a[], int n) {
+    int i, j, flag = 0; // adding var flag makes it adaptive
+    for (i = 0; i < n - 1; ++i) {
+        flag = 0;
+        for (j = 0; j < n - 1; ++j) {
+            if (a[j] > a[j + 1])
+                swap(&a[j], &a[j + 1]);
+            flag = 1;
+        }
+        if (flag == 0)
+            break;
+    }
+}
+
+int main() {
+
+    int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
+    bubble(a, n);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
---
+++ 20_sortingTechniquee/countSort.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+// todo there is a bug
+int findMax(int a[], int n) {
+    int max = INT_MIN;
+    int i;
+    for (int i = 0; i < n; ++i) {
+        if (a[i] > max)
+            max = a[i];
+    }
+    return max;
+}
+
+void countSort(int a[], int n) {
+    int i, j, max, *c;
+    max = findMax(a, n);
+    c = (int *) malloc(sizeof(int) * max + 1);
+    for (i = 0; i < max + 1; ++i) {
+        c[i] = 0;
+    }
+    for (i = 0; i < n; ++i) {
+        c[a[i]]++;
+    }
+    i = 0;
+    j = 0;
+    while (j < max + 1) {
+        if (c[j] > 0) {
+            a[i] = j;
+            c[j]--;
+        } else
+            j++;
+    }
+
+}
+
+
+int main() {
+
+    int a[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2}, n = 10;
+    countSort(a, n);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
