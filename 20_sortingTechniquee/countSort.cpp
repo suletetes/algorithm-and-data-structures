2f954ffb18d41160e6cb88ef6ee1996e6e5f89fb
suleiman <sulea841@gmail.com>
2023-06-09 18:17:34 +0100

    section 20 sorting technique

--- 20_sortingTechniquee/countSort.cpp
+++ 20_sortingTechniquee/countSort.cpp
@@ -3,13 +3,34 @@
 
 
 int findMax(int a[], int n) {
-    int max = INT32_MIN;
+    int max = INT_MIN;
     int i;
     for (int i = 0; i < n; ++i) {
         if (a[i] > max)
             max = a[i];
     }
     return max;
+}
+
+void countSort(int a[], int n) {
+    int i, j, max, *c;
+    c = (int *) malloc(sizeof(int) * max + 1);
+    for (i = 0; i < max + 1; ++i) {
+        c[i] = 0;
+    }
+    for (i = 0; i < n; ++i) {
+        c[a[i]]++;
+    }
+    i = 0;
+    j = 0;
+    while (j < max + 1) {
+        if (c[j] > 0) {
+            a[i] = j;
+            c[j]--;
+        } else
+            j++;
+    }
+
 }
 
 
