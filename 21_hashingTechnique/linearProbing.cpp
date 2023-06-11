ffcd6f824a0cfdb9d3fd988bc2c0e27ed1405013
suleiman <sulea841@gmail.com>
2023-06-11 15:36:44 +0100

    section 21 Hashing Technique

--- 21_hashingTechnique/linearProbing.cpp
+++ 21_hashingTechnique/linearProbing.cpp
@@ -6,11 +6,27 @@ int hash(int key) {
     return key % SIZE;
 }
 
+int probe(int h[], int key) {
+    int index = hash(key);
+    int i = 0;
+    while (h[(index + i)] % SIZE != 0)
+        i++;
+    return (index + i) % SIZE;
+}
+
 void insert(int h[], int key) {
     int index = hash(key);
+    if (h[index] != 0)
+        index = probe(h, key);
+    h[index] = key;
 }
 
 int main() {
+    int ht[10] = {0};
+    insert(ht, 12);
+    insert(ht, 25);
+    insert(ht, 35);
+    insert(ht, 26);
 
 
     return 0;
