f36448f42be9671867395646f1a1a889560d7b56
suleiman <sulea841@gmail.com>
2023-06-11 14:20:08 +0100

    section 22 graphs

--- 22_graphs/BFSDFS.cpp
+++ 22_graphs/BFSDFS.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include "stdio.h"
+#include "queue.h"
 
 //bfs(breadth first search) dfs (depth first search)
+void bfs(int g[][7], int start, int n) {
+    int i = start;
+    struct Queue q;
+    int visited[7] = {0};
+
+    printf("%d ", i);
+    visited[i] = 1;
+    enqueue(q, i);
+    while (!isEmpty()) {
+        i = dequeue();
+        for (int j = 1; j < n; ++j) {
+            if (g[i][j] == 1 && visited[j] == 0) {
+                printf("%d ", j);
+                visited[j] = 1;
+                enqueue(j);
+            }
+        }
+    }
+};
+
 int main() {
     int g[7][7] = {
             {0, 0, 0, 0, 0, 0, 0},
