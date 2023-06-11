5f3724d003f998885c5e2b4bda1f7f19cd780a83
suleiman <sulea841@gmail.com>
2023-06-11 15:17:38 +0100

    section 21 Hashing Technique

--- 21_hashingTechnique/doubleHashing.cpp
+++ 21_hashingTechnique/doubleHashing.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #define SIZE 10
+#define PRIME 7
 
 using namespace std;
 
@@ -19,20 +20,24 @@ int Hash(int key){
     return key % SIZE;
 }
 
-int QuadraticProbe(int H[], int key){
+int PrimeHash(int key){
+    return PRIME - (key % PRIME);
+}
+
+int DoubleHash(int H[], int key){
     int idx = Hash(key);
     int i = 0;
-    while (H[(idx+i*i) % SIZE] != 0){
+    while (H[(Hash(idx) + i * PrimeHash(idx)) % SIZE] != 0){
         i++;
     }
-    return (idx + i*i) % SIZE;
+    return (idx + i * PrimeHash(idx)) % SIZE;
 }
 
 void Insert(int H[], int key){
     int idx = Hash(key);
 
     if (H[idx] != 0){
-        idx = QuadraticProbe(H, key);
+        idx = DoubleHash(H, key);
     }
     H[idx] = key;
 }
@@ -40,19 +45,19 @@ void Insert(int H[], int key){
 int Search(int H[], int key){
     int idx = Hash(key);
     int i = 0;
-    while (H[(idx+i*i) % SIZE] != key){
+    while (H[(Hash(idx) + i * PrimeHash(idx)) % SIZE] != key){
         i++;
-        if (H[(idx + i*i) % SIZE] == 0){
+        if (H[(Hash(idx) + i * PrimeHash(idx)) % SIZE] == 0){
             return -1;
         }
     }
-    return (idx + i*i) % SIZE;
+    return (Hash(idx) + i * PrimeHash(idx)) % SIZE;
 }
 
 
 int main() {
 
-    int A[] = {26, 30, 45, 23, 25, 43, 74, 19, 29};
+    int A[] = {5, 25, 15, 35, 95};
     int n = sizeof(A)/sizeof(A[0]);
     Print(A, n, " A");
 
