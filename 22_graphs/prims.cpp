b6bca3c1c32a959400f21180f2754ae90ff4dd20
suleiman <sulea841@gmail.com>
2023-06-11 14:34:24 +0100

    section 22 graphs

--- 22_graphs/prims.cpp
+++ 22_graphs/prims.cpp
@@ -1,3 +1,18 @@
-//
-// Created by suleiman on 10/06/2023.
-//
+#define V 8
+#define I 32767
+
+
+int main() {
+
+    int cost[V][V]{
+            {I, I,  I,  I,  I,  I,  I,  I},
+            {I, I,  25, I,  I,  I,  5,  I},
+            {I, 25, I,  12, I,  I,  I,  10},
+            {I, I,  12, I,  8,  I,  I,  I},
+            {I, I,  I,  8,  I,  16, I,  14},
+            {I, I,  I,  I,  16, I,  20, 18},
+            {I, 5,  I,  I,  I,  20, I,  I},
+            {I, I,  10, I,  14, 18, I,  I},
+    };
+
+}
