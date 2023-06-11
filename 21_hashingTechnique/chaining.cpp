781f3c4d426abe137dd640abce1971350cc78a77
suleiman <sulea841@gmail.com>
2023-06-11 15:24:55 +0100

    section 21 Hashing Technique

--- 21_hashingTechnique/chaining.cpp
+++ 21_hashingTechnique/chaining.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include "stdio.h"
+#include "chains.h"
+
+int hash(int key) {
+    return key % 10;
+
+}
+
+void insert(struct Node *h[], int key) {
+    int index = hash(key);
+    SortedInsert(&h[index], key);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    struct Node *HT[10];
+    for (int i = 0; i < 10; ++i) {
+        HT[i] = NULL;
+    }
+    insert(HT, 12);
+    insert(HT, 22);
+    insert(HT, 42);
 }
--- 21_hashingTechnique/linearProbing.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by suleiman on 11/06/2023.
-//
