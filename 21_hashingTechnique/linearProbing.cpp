5a4d0a51a82d76dced440542bfd384c8ab193aa1
suleiman <sulea841@gmail.com>
2023-06-11 15:33:36 +0100

    section 21 Hashing Technique

---
+++ 21_hashingTechnique/linearProbing.cpp
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+#define SIZE 10
+
+int hash(int key) {
+    return key % SIZE;
+}
+
+void insert(int h[], int key) {
+    int index = hash(key);
+}
+
+int main() {
+
+
+    return 0;
+}
